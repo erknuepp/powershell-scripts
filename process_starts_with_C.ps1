ceceaf327709921b73f52252f663211bd3b34068
Edward R Knueppel Jr <35438229+erknuepp@users.noreply.github.com>
2021-10-06 20:13:28 -0500

    Create process_starts_with_C.ps1
    Initial commit

---
+++ process_starts_with_C.ps1
@@ -0,0 +1,11 @@
+<# 
+  Author: Edward Knueppel Jr. edward.knueppel.jr@gmail.com
+  Outputs processes (Id, Name, Handles) that start with "C" (case-sensetive) ordered by Id Descending
+#> 
+
+$process_array = @()
+foreach ($process in Get-Process) {
+    $process_array += $process
+    }
+
+$process_array | Where {$_.Name.Substring(0,1) -cLike "C"} | Sort id -Descending | Select Id, Name, Handles | FT  
