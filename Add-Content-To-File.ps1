32370ee12c3c298f155d4ecff83adf5a11b7c223
Edward R Knueppel Jr <35438229+erknuepp@users.noreply.github.com>
2021-10-08 20:43:47 -0500

    Create Add-Content-To-File.ps1

---
+++ Add-Content-To-File.ps1
@@ -0,0 +1,3 @@
+<#E.g. Add-Content C:\testfolder\test.txt ‘adding content’
+
+The above cmdlet appends the content specified to the test file if it exists, else it automatically creates the file and adds the content to it.#>
