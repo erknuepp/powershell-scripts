fbdbd97b357052c1716c94f7de0c0cd872988dad
Edward R Knueppel Jr <35438229+erknuepp@users.noreply.github.com>
2021-10-08 20:45:05 -0500

    Update Add-Content-To-File.ps1
    Adds link to website.

--- Add-Content-To-File.ps1
+++ Add-Content-To-File.ps1
@@ -1,3 +1,7 @@
-<#E.g. Add-Content C:\testfolder\test.txt ‘adding content’
+<#
+
+https://www.educba.com/add-content-in-powershell
+
+E.g. Add-Content C:\testfolder\test.txt ‘adding content’
 
 The above cmdlet appends the content specified to the test file if it exists, else it automatically creates the file and adds the content to it.#>
